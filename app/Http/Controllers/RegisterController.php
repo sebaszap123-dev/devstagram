a4a27b8a54decd92fa6fc1a3531c326ee1c96c94
sebaszap123-dev <sebaszap123@gmail.com>
2023-10-21 15:53:33 -0600

    fixed when user register go to posts.index

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -41,6 +41,6 @@ class RegisterController extends Controller
         // ]);
         // Another auth form
         auth()->attempt($request->only('email', 'password'));
-        return redirect()->route('posts.index');
+        return redirect()->route('posts.index', auth()->user()->username);
     }
 }
