50babd2d9ea1bed0c82baeb19acba8087b2d0b87
sebaszap123-dev <sebaszap123@gmail.com>
2023-10-21 11:59:50 -0600

    terminado pirmer version muro

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,30 @@
 @extends('layouts.app')
 
 @section('title')
-    My Account
+    Perfil: {{ $user->username }}
 @endsection
 
 @section('content')
     <div class="flex justify-center">
-        <div class="w-full md:w-8/12 lg:w-6/12 md:flex">
-            <div class="md:w-8/12 lg:w-6/12 px-5">
+        <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
+            <div class="w-8/12 lg:w-6/12 px-5">
                 <img src="{{ asset('usuario.svg') }}" alt="foto de perfil usuario">
             </div>
-            <div class="md:w-8/12 lg:w-6/12 px-5">
-                <p class="text-gray-700 text-2xl"> {{ auth()->user()->username }}</p>
+            <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:items-start md:py-10 py-10">
+                <p class="text-gray-700 text-2xl"> {{ $user->username }}</p>
+
+                <p class="text-gray-800 text-sm mb-4 font-bold mt-5">
+                    0
+                    <span class="font-normal">Seguidores</span>
+                </p>
+                <p class="text-gray-800 text-sm mb-4 font-bold">
+                    0
+                    <span class="font-normal">Siguendo</span>
+                </p>
+                <p class="text-gray-800 text-sm mb-4 font-bold">
+                    0
+                    <span class="font-normal">Posts</span>
+                </p>
             </div>
         </div>
     </div>
